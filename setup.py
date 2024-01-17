4681a96a47886a43e2c98d56b8ddf0a2fbd16858
Basi10 <basilelbirru@gmail.com>
2024-01-17 17:36:45 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='rag-prompt-engineering',
+    version='1.0.0',
+    description='Prompt Engineering with RAG Technique',
+    author='Basilel Birru',
+    author_email='basilelbirru@gmail.com',
+    url='https://github.com/Basi10/rag-prompt-engineering.git',
+)
